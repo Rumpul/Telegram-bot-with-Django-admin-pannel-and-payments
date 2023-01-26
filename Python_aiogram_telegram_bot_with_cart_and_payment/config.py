45f71a9f18f4e33ba29fb3ff89984bcddbbcaf3a
Roman <109575766+Rumpul@users.noreply.github.com>
2023-01-26 11:50:30 +0300

    Update config.py

--- Python_aiogram_telegram_bot_with_cart_and_payment/config.py
+++ Python_aiogram_telegram_bot_with_cart_and_payment/config.py
@@ -3,4 +3,4 @@
 token = ""  # TODO Fill in your telegrambot token
 yootoken = ""  # TODO Fill in your yookassa token
 sbertoken = ""  # TODO Fill in your sber token
-# token = os.environ.get("") # Yse this when you put your token in environment variables
+# token = os.environ.get("") # Use this when you put your token in environment variables
